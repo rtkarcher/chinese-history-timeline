a123d39edc09292bab670dab112368059b97179c
Rainer Karcher <Rainer.Karcher@Cerner.com>
2014-12-10 12:14:33 -0600

    Directory reorganization

---
+++ dynasties.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-touch-fullscreen" content="yes">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    
+    <!-- HTML5 shim, for IE6-8 support of HTML elements--><!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+
+    <!-- Style-->
+    <style>
+      html, body {
+      height:100%;
+      padding: 0px;
+      margin: 0px;
+      }
+      #timeline-embed img {
+      margin-top:15px;
+      }
+    </style>
+    
+</head>
+<body>
+    <!-- BEGIN Timeline Embed -->
+    <div id="timeline-embed"></div>
+    <script type="text/javascript">
+      var timeline_config = {
+       width: "100%",
+       height: "100%",
+       source: 'https://docs.google.com/spreadsheet/pub?key=0AghPi8JsmBSydFRwT19fbGx3UXpIWmNiYzRNREY0S1E&output=html',
+       font: 'Georgia-Helvetica'
+      }
+    </script>
+    <script type="text/javascript" src="build/js/storyjs-embed.js"></script>
+    <!-- END Timeline Embed-->
+</body>
+</html>
